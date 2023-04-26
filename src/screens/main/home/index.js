bf45706f1de0f2fa1868cc71d5ff929c2924ae67
Elite <petpanda0057@gmail.com>
2023-04-26 18:47:40 +0300

    feat: add how it works section in home page

--- src/screens/main/home/index.js
+++ src/screens/main/home/index.js
@@ -7,6 +7,7 @@ import Layout from '../../../layouts';
 // components
 // sections
 import HomeHero from '../../../sections/main/home/homeHero';
+import HowItWork from '../../../sections/main/home/howItWork';
 
 // ----------------------------------------------------------------------
 
@@ -14,6 +15,7 @@ export default function Home() {
   return (
     <Layout variant="main">
       <HomeHero />
+      <HowItWork />
     </Layout>
   );
 }
