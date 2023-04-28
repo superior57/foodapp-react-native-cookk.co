1652567cf83b894a63b12da29d9c49aaa2154f1e
Elite <petpanda0057@gmail.com>
2023-04-28 05:02:16 +0300

    feat: add yup and formik package set validation in contact us page

--- src/sections/main/contactUs/form/index.js
+++ src/sections/main/contactUs/form/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as yup from 'yup';
+import {Formik} from 'formik';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 // react-native
@@ -18,7 +20,7 @@ import Button from '../../../../components/button';
 // sections
 // routes
 // theme
-import {PRIMARY, SECONDARY} from '../../../../theme';
+import {ERROR, PRIMARY, SECONDARY} from '../../../../theme';
 // redux
 
 // ----------------------------------------------------------------------
@@ -49,6 +51,19 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
+
+  error: {color: ERROR.main},
+});
+
+// ----------------------------------------------------------------------
+
+const loginValidationSchema = yup.object().shape({
+  full_name: yup.string().required('Full name is required'),
+  email: yup
+    .string()
+    .email('Please enter valid email')
+    .required('Email address is required'),
+  message: yup.string().required('Message is required'),
 });
 
 // ----------------------------------------------------------------------
@@ -57,34 +72,73 @@ export default function ContactUsForm() {
   return (
     <ImageBackground
       source={require('../../../../assets/images/contactUs/form.png')}>
-      <Stack style={styles.form}>
-        <Typography variant="h5" color={SECONDARY.main}>
-          Ask a question
-        </Typography>
-        <TextInput style={styles.input} placeholder="Full name" />
-        <TextInput style={styles.input} placeholder="Email address" />
-        <TextInput
-          multiline
-          style={styles.multilineInput}
-          textAlignVertical="top"
-          numberOfLines={8}
-          placeholder="Your message"
-        />
-        <Stack direction="row" justify="between">
-          <Button paddingHorizontal={40} fontWeight={900} borderRadius={100}>
-            Ask a question
-          </Button>
-          <IconButton
-            icon={
-              <TouchableOpacity onPress={() => {}}>
-                <Stack style={styles.iconButton}>
-                  <Icon name="phone" size={15} color={'white'} />
-                </Stack>
-              </TouchableOpacity>
-            }
-          />
-        </Stack>
-      </Stack>
+      <Formik
+        validationSchema={loginValidationSchema}
+        initialValues={{full_name: '', email: '', message: ''}}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+          <Stack style={styles.form}>
+            <Typography variant="h5" color={SECONDARY.main}>
+              Ask a question
+            </Typography>
+            <TextInput
+              name="full_name"
+              placeholder="Full name"
+              style={styles.input}
+              onChangeText={handleChange('full_name')}
+              onBlur={handleBlur('string')}
+              value={values.full_name}
+            />
+            {errors.full_name && (
+              <Typography color={ERROR.main}>{errors.full_name}</Typography>
+            )}
+            <TextInput
+              name="email"
+              placeholder="Email address"
+              style={styles.input}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              keyboardType="email-address"
+            />
+            {errors.email && (
+              <Typography color={ERROR.main}>{errors.email}</Typography>
+            )}
+            <TextInput
+              name="message"
+              multiline
+              style={styles.multilineInput}
+              textAlignVertical="top"
+              numberOfLines={8}
+              placeholder="Your message"
+              onChangeText={handleChange('message')}
+              onBlur={handleBlur('string')}
+              value={values.message}
+            />
+            {errors.message && (
+              <Typography color={ERROR.main}>{errors.message}</Typography>
+            )}
+            <Stack direction="row" justify="between">
+              <Button
+                onPress={handleSubmit}
+                paddingHorizontal={40}
+                fontWeight={900}
+                borderRadius={100}>
+                Ask a question
+              </Button>
+              <IconButton
+                icon={
+                  <TouchableOpacity onPress={() => {}}>
+                    <Stack style={styles.iconButton}>
+                      <Icon name="phone" size={15} color={'white'} />
+                    </Stack>
+                  </TouchableOpacity>
+                }
+              />
+            </Stack>
+          </Stack>
+        )}
+      </Formik>
     </ImageBackground>
   );
 }
